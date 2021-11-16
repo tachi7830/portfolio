44e7dfbd631ef360bfdddce25caedbb922edc59e
tachi7830 <xwsmilexw@gmail.com>
2021-11-16 16:26:06 +0000

    新規登録、ログインページのレイアウト修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @articles = @user.articles.page(params[:page]).per(5).order("id DESC")
-    @favourites = Favourite.where(user_id: current_user.id)
+    @favourites = Favourite.where(user_id: current_user.id).page(params[:page]).per(5).order("id DESC")
   end
 
   def edit
