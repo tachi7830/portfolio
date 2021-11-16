07450364f073bac95a28c3c9e009da541016957f
tachi7830 <xwsmilexw@gmail.com>
2021-11-16 08:08:47 +0000

    ユーザーマイページのレイアウト修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to root_path
+    end
   end
   
   def update
