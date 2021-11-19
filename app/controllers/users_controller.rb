4c3415a472d58abec402fbc9b87e9c800292f5f3
tachi7830 <xwsmilexw@gmail.com>
2021-11-19 07:36:57 +0000

    お店の商品一覧ページ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @articles = @user.articles.page(params[:page]).per(5).order("id DESC")
-    @favourites = Favourite.where(user_id: current_user.id).page(params[:page]).per(5).order("id DESC")
+    if user_signed_in?
+      @favourites = Favourite.where(user_id: current_user.id).page(params[:page]).per(5).order("id DESC")
+    end
   end
 
   def edit
@@ -13,19 +15,19 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
     redirect_to user_path(user.id)
   end
 
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :image, :email )
   end
-  
+
 end
 
