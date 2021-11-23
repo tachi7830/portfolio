b2fed9f27f8f40cf04dda6ba67d7245883077c8a
tachi7830 <xwsmilexw@gmail.com>
2021-11-23 15:02:15 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
 
   root :to => "homes#top"
-  
+
   devise_for :users,  controllers: {
     registrations: 'users/registrations'
   }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  get 'shops/search' => 'shops#search'
 
   resources :articles do
     resource :goods, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:show, :edit, :update]
   get "mail_check", to: "users#mail_check"
-  
+
   resources :shops, only: [:index, :show] do
     resource :favourites, only: [:create, :destroy]
     resources :breads, only: [:index]
   end
-  get 'shops/search' => 'shops#search'
-  
+
   resources :contacts, only: [:new, :create]
   get "contacts/thanks", to: "contacts#thanks"
 
