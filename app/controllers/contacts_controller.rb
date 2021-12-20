a33ef1810c1c89caba6b60c6f9370ef577d3a8e7
tachi7830 <xwsmilexw@gmail.com>
2021-12-20 14:11:16 +0000

    contacts_controllerコメント追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      #お問合せ内容が保存できたらsend_mailのアドレス宛にメールを送信
       ContactMailer.send_mail(@contact).deliver
       redirect_to contacts_thanks_path
     else
