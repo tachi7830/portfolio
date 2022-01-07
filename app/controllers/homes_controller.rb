f0a847aeb3831df6780b715994c1d185eb2e716c
tachi7830 <xwsmilexw@gmail.com>
2022-01-07 05:16:01 +0000

    いいねランキングコントローラー修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,12 +2,8 @@ class HomesController < ApplicationController
 
   def top
     @articles = Article.order("id DESC").page(params[:page]).per(3)
-    @week_ranks = Article.find(Good.group(:article_id).where(created_at: Time.current.all_week).limit(5).order('count(article_id) desc').pluck(:article_id))
-    #Good.group(:article_id)=Goodモデルのarticle_idが同じものをまとめる
-    #Time.current=現在の日時を表示するメソッド
-    #.all_week=1週間のデータを取得
-    #order('count(article_id) desc')=まとめたものをarticle_idの多い順に並び替える
-    #pluck(:article_id)=article_idで値を取りだす
+    @week_ranks = Article.create_week_ranks
+    #@week_ranks = Article.find(Good.group(:article_id).where(created_at: Time.current.all_week).limit(5).order('count(article_id) desc').pluck(:article_id))
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -13,7 +13,16 @@ class Article < ApplicationRecord
   def gooded_by?(user)
     goods.where(user: user).exists?
   end
-
+  
+  def self.create_week_ranks
+    Article.find(Good.group(:article_id).where(created_at: Time.current.all_week).limit(5).order('count(article_id) desc').pluck(:article_id))
+  end
+  #Good.group(:article_id)=Goodモデルのarticle_idが同じものをまとめる
+  #Time.current=現在の日時を表示するメソッド
+  #.all_week=1週間のデータを取得
+  #order('count(article_id) desc')=まとめたものをarticle_idの多い順に並び替える
+  #pluck(:article_id)=article_idで値を取りだす
+  
   validates :shop_id, presence: true
   validates :images, presence: true
   validates :prefecture_code, presence: true
